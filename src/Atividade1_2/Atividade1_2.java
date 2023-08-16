9db282544d005e361274fe42322f02f2f798df7c
SusanaaMuniz20 <susana.anadrade.muniz@gmail.com>
2023-08-16 17:41:26 -0300

    first commit

---
+++ src/Atividade1_2/Atividade1_2.java
@@ -0,0 +1,26 @@
+package Atividade1_2;
+
+import java.util.Scanner;
+
+public class Atividade1_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner leia = new Scanner (System.in);
+		
+		int renda;
+		
+		System.out.println("Por favor digite sua renda: ");
+		renda = leia.nextInt();
+		
+		System.out.println("\nO valor da sua renda é: " + renda);
+		
+		
+		
+		leia.close();
+		
+		
+		
+	}
+
+}
